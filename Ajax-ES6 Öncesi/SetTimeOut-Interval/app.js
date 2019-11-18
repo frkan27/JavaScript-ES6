fef014f6a25ddf068d1b591c2bc4baefdcb3e862
Furkan Kılıç <furkankilicytu@gmail.com>
2019-11-18 23:21:24 +0300

    settimelar

---
+++ Ajax-ES6 Öncesi/SetTimeOut-Interval/app.js
@@ -0,0 +1,26 @@
+
+//Set TimeOut
+//Belli işlemi belli saniye sonra başlatabiliyrum
+
+
+// setTimeout(function(){
+//     console.log("naber");
+// }, 2000);
+
+
+// //Set Interval
+// // aralıklarla tekrar tekrar çalıştırıyor
+// let i=0;
+
+// let value=setInterval(function(){
+// i++;
+// console.log("sayı",i);
+
+// }
+// ,2000
+// );
+
+// document.getElementById("btn").addEventListener("click",function(){
+// clearInterval(value);
+// });
+
